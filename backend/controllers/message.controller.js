de8a1ffecbf8ad053239d91f0fb89754608236ac
Ibrahim Hafez <ibrahimhafez1@yahoo.com>
2024-05-28 22:08:25 +0300

    app completed

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,60 +1,62 @@
-import Conversation from "../models/conversation.model.js"
-import Message from "../models/message.model.js"
+import Conversation from "../models/conversation.model.js";
+import Message from "../models/message.model.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
 export const sendMessage = async (req, res) => {
     try {
-        const { message } = req.body
-        const { id: receiverId } = req.params
-        const senderId = req.user._id
+        const { message } = req.body;
+        const { id: receiverId } = req.params;
+        const senderId = req.user._id;
 
         let conversation = await Conversation.findOne({
-            participants: {
-                $all: [senderId, receiverId]
-            }
-        })
+            participants: { $all: [senderId, receiverId] },
+        });
 
         if (!conversation) {
             conversation = await Conversation.create({
-                participants: [senderId, receiverId]
-            })
+                participants: [senderId, receiverId],
+            });
         }
 
         const newMessage = new Message({
             senderId,
             receiverId,
-            message
-        })
+            message,
+        });
 
         if (newMessage) {
-            conversation.messages.push(newMessage._id)
+            conversation.messages.push(newMessage._id);
         }
 
-        Promise.all([newMessage.save(), conversation.save()])
+        await Promise.all([conversation.save(), newMessage.save()]);
 
-        res.status(201).json({ newMessage })
+        const receiverSocketId = getReceiverSocketId(receiverId);
+        if (receiverSocketId) {
+            io.to(receiverSocketId).emit("newMessage", newMessage);
+        }
 
+        res.status(201).json(newMessage);
     } catch (error) {
-        res.status(500).json({ message: error.message })
+        console.log("Error in sendMessage controller: ", error.message);
+        res.status(500).json({ error: "Internal server error" });
     }
-}
+};
 
 export const getMessages = async (req, res) => {
     try {
-        const senderId = req.user._id
-        const { id: userToChatId } = req.params
+        const { id: userToChatId } = req.params;
+        const senderId = req.user._id;
+
         const conversation = await Conversation.findOne({
-            participants: {
-                $all: [senderId, userToChatId]
-            }
-        }).populate('messages')
+            participants: { $all: [senderId, userToChatId] },
+        }).populate("messages");
+        if (!conversation) return res.status(200).json([]);
 
-        if (!conversation) {
-            return res.status(404).json({ message: 'Conversation not found' })
-        }
+        const messages = conversation.messages;
 
-        res.status(200).json(conversation.messages)
-
+        res.status(200).json(messages);
     } catch (error) {
-        res.status(500).json({ message: error.message })
+        console.log("Error in getMessages controller: ", error.message);
+        res.status(500).json({ error: "Internal server error" });
     }
-}
+};
--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,13 +1,14 @@
 import User from "../models/user.model.js"
 
-export const getUsersForSideBar = async (req, res) => {
+export const getUsersForSidebar = async (req, res) => {
     try {
-        const loggedinUserId = req.user._id
+        const loggedInUserId = req.user._id;
 
-        const filteredUsers = await User.find({ _id: { $ne: loggedinUserId } }).select("-password")
+        const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
 
-        res.status(200).json(filteredUsers)
+        res.status(200).json(filteredUsers);
     } catch (error) {
-        res.status(500).json({ message: error.message })
+        console.error("Error in getUsersForSidebar: ", error.message);
+        res.status(500).json({ error: "Internal server error" });
     }
-}
+};
